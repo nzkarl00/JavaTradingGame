1d4777861480c7aedcc967e73733b0f442d9fd9c
Karl Moore <krm97@uclive.ac.nz>
2021-04-07 19:02:22 +1200

    Update Island.java Added foundations for this class, will need to be updates with routes and stores.

--- src/Island.java
+++ src/Island.java
@@ -8,7 +8,29 @@
  * */
 
 public class Island {
-
-	//todo lmao
-
-}
+	private String name; //Name of the island.
+	private int xcoord; //X co-ordinate of the island.
+	private int ycoord; //Y co-ordinate of the island.
+	private ArrayList<IslandRoutes> routes = new ArrayList<IslandRoutes>(); //ArrayList containing all possible routes from the island.
+	
+	public Island(String tempname, int tempx, int tempy) { //Constructor for creating new islands.
+		name = tempname;
+		xcoord = tempx;
+		ycoord = tempy;
+	}
+	
+	public int getX() {
+		return xcoord;
+	}
+	
+	public int getY() {
+		return ycoord;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public String toString() {
+		return (name + ", found at (" + xcoord + "," + ycoord + ")");
+	}
