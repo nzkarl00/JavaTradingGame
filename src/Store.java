e810dc73439a661a2439bfab645796f022ac2f63
Karl Moore <krm97@uclive.ac.nz>
2021-05-18 21:32:53 +1200

    Added transactions

--- src/Store.java
+++ src/Store.java
@@ -36,6 +36,17 @@ public class Store {
 		System.out.println("");
 	}
 	
+	public int getPrice(Item good, boolean buying) {
+		double priceMod = stock.get(good);
+		int adjustedPrice = 0;
+		if (buying == true) {
+			adjustedPrice = (int) (good.getBaseValue() * (1.55 - (priceMod / 20)));
+		} else {
+			adjustedPrice = (int) (good.getBaseValue() * (1.45 - (priceMod / 20)));
+		}
+		return adjustedPrice;
+	}
+	
 	public void getBuyableItems() {
 		System.out.println("Buyable Inventory:");
 		for(Item i : buyables) {
@@ -44,12 +55,67 @@ public class Store {
 		System.out.println("");
 	}
 	
-	public void stockItem(Item thingy, int quantity, boolean isSellable) {
-		stock.put(thingy, quantity);
-		if (isSellable) {
-			sellables.add(thingy);
+	public void createItem(Item item, int quantity) {
+		stock.put(item, quantity);
+		if (stock.get(item) < 5) {
+			sellables.add(item);
+		} else if (stock.get(item) > 15) {
+			buyables.add(item);
 		} else {
-			buyables.add(thingy);
+			sellables.add(item);
+			buyables.add(item);
+		}
+	}
+	
+	public void addItem(Item item, int quantity) {
+		stock.put(item, stock.get(item) + quantity);
+		if (stock.get(item) < 5) {
+			if (sellables.contains(item) == false) {
+				sellables.add(item);
+			}
+			if (buyables.contains(item) == true) {
+				buyables.remove(item);
+			}
+		} else if (stock.get(item) > 15) {
+			if (buyables.contains(item) == false) {
+				buyables.add(item);
+			}
+			if (sellables.contains(item) == true) {
+				sellables.remove(item);
+			}
+		} else {
+			if (sellables.contains(item) == false) {
+			sellables.add(item);
+			}
+			if (buyables.contains(item) == false) {
+			buyables.add(item);
+			}
+		}
+	}
+	
+	public void removeItem(Item item, int quantity) {
+		stock.put(item, stock.get(item) - quantity);
+		if (stock.get(item) < 5) {
+			if (sellables.contains(item) == false) {
+				sellables.add(item);
+			}
+			if (buyables.contains(item) == true) {
+				buyables.remove(item);
+			}
+		} else if (stock.get(item) > 15) {
+			if (buyables.contains(item) == false) {
+				buyables.add(item);
+			}
+			if (sellables.contains(item) == true) {
+				sellables.remove(item);
+			}
+		} else {
+			if (sellables.contains(item) == false) {
+			sellables.add(item);
+			}
+			if (buyables.contains(item) == false) {
+			buyables.add(item);
+			}
 		}
 	}
 	
@@ -84,15 +150,15 @@ public class Store {
 				//buy
 				message = "Enter a number between 1 and " + buyables.size() + " to select an item to buy";
 				itemIndex = ui.queryIntBetweenRange(message, 1, buyables.size());
-				itemIndex -= 1;
-				ui.showMessage("Buy item " + buyables.get(itemIndex).getName());
+				Item selectedItem = buyables.get(itemIndex - 1);
+				purchaseItem(selectedItem, player, ui);
 				break;
 			case 1:
 				//sell
 				message = "Enter a number between 1 and " + sellables.size() + " to select an item to sell";
 				itemIndex = ui.queryIntBetweenRange(message, 1, sellables.size());
-				itemIndex -= 1;
-				ui.showMessage("Sell item " + sellables.get(itemIndex).getName());
+				Item forSale = sellables.get(itemIndex - 1);
+				sellItem(forSale, player, ui);
 				break;
 			case 2:
 				ui.showMessage("Exiting store.");
@@ -102,5 +168,36 @@ public class Store {
 
 		handleStoreOptions(player, ui);
 	}
-
+	
+	public void purchaseItem(Item item, Player player, UI ui) {
+		String message = ("Enter quantity, " + stock.get(item) + " in stock for $" + getPrice(item, true) + " each. You have $" + player.getMoney() + ".");
+		int purchaseQuantity = ui.queryIntBetweenRange(message, 1, stock.get(item));
+		int total = (int) (getPrice(item, true) * purchaseQuantity);
+		if (player.getMoney() < total) {
+			System.out.println("Insufficient funds.");
+		} else {
+			String purchaseMessage = "Successfully purchased " + purchaseQuantity + " " + item.getName() + " for $" + total + ".";
+			total *= -1;
+			player.transferMoney(total, ui);
+			player.getShip().addItem(item, purchaseQuantity);
+			System.out.println(purchaseMessage + " You have $" + player.getMoney() + " remaining.");
+			removeItem(item, purchaseQuantity);
+		}
+	}
+	
+	public void sellItem(Item item, Player player, UI ui) {
+		String message = ("Enter quantity, purchasing price of " + item.getName() + " is " + getPrice(item, false) + " per unit.");
+		int quantityOwned = player.getShip().playerInventory.get(item);
+		if (quantityOwned == 0) {
+			System.out.println("You don't have any " + item.getName());
+		} else {
+			int saleQuantity = ui.queryIntBetweenRange(message, 0, quantityOwned);
+			int total = (int) (getPrice(item, false) * saleQuantity);
+			player.transferMoney(total, ui);
+			player.getShip().removeItem(item, saleQuantity);
+			System.out.println("Successfully sold " + saleQuantity + " " + item.getName() + " for $" + total + ".");
+			System.out.println("You now have $" + player.getMoney() + ".");
+			addItem(item, saleQuantity);
+		}
+	}
 }
